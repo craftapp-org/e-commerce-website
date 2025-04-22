9dd27c1935298362eaed2f2d92fbde3bc5b74137
Zabih Keraam <165507052+Zabihkeraam1@users.noreply.github.com>
2025-04-22 19:45:02 +0430

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,16 +1,8 @@
 import type { NextConfig } from "next";
-/** @type {import('next').NextConfig} */
+
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  // Enable the webpack alias configuration
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, 'src'),
-      '@/lib': path.resolve(__dirname, 'src/lib')
-    }
-    return config
-  }
-}
+  output: 'standalone', 
+  /* config options here */
+};
 
 export default nextConfig;
